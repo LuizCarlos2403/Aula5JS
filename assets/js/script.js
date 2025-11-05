3500065ed3045864c30c9a9ee7e8fc5592766454
Luiz Carlos Evaristo Leandro <luizcarlos24032009@gmail.com>
2025-11-05 08:27:58 -0300

    Aula 5Js
    Atividade completa de Arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,3 +158,105 @@ alert("Os nomes digitados foram: " + nicks.join(", "));
 
 //Exercício 8 — Inverter a ordem
 //Peça 5 números, armazene em um array e mostre-os na ordem inversa.
+
+let numeros6 = [];
+
+for (let i = 0; i < 5; i++) {
+  let numero = Number(prompt(`Digite o ${i + 1}º número:`));
+  numeros6.push(numero);
+}
+
+alert("Números na ordem digitada: " + numeros6.join(", "));
+alert("Números na ordem inversa: " + numeros6.reverse().join(", "));
+
+
+
+
+
+
+
+
+//Exercício 9 — Adicionar nomes com push()
+//Peça ao usuário para digitar 5 nomes e adicione cada um ao final do array 
+// usando push(). Depois mostre todos os nomes digitados.
+
+
+let nom = [];
+
+for (let i = 0; i < 5; i++) {
+  let nome = prompt(`Digite o ${i + 1}º nome:`);
+  nom.push(nome);
+}
+
+alert("Os nomes digitados foram: " + nom.join(", "));
+
+
+
+
+
+
+
+//Exercício 10 — Inserir números no início com unshift()
+//Peça ao usuário para digitar 5 números e adicione cada um no início do array usando unshift().Mostre o array final.
+
+let numeros7 = [];
+
+for (let i = 0; i < 5; i++) {
+  let numero = Number(prompt(`Digite o ${i + 1}º número:`));
+  numeros7.unshift(numero);
+}
+
+alert("Números na ordem digitada: " + numeros7.join(", "));
+
+
+
+
+
+
+//Exercício 12 — Remover o primeiro elemento com shift()
+//Enunciado:
+//Crie um array com 5 frutas.Mostre a lista e depois remova a primeira fruta com shift().
+//Mostre novamente o array atualizado.
+
+
+let frutas = ["melancia", "goiba", "uva", "laranja", "abacate"];
+
+alert("Lista de frutas: " + frutas.join(", "));
+
+frutas.shift();
+
+alert("Lista atualizada (sem a primeira fruta): " + frutas.join(", "));
+
+
+
+
+//Exercício 13 — Misturando tudo (push(), unshift(), pop(), shift())
+//Enunciado:
+//Crie um array vazio.
+//1. Peça 3 nomes e adicione no final com push().
+//2. Peça 3 nomes e adicione no início com unshift().
+//3. Depois remova o primeiro e o último nome usando shift() e pop().
+//Mostre o array final.
+
+
+let nomess = [];
+
+
+for (let i = 0; i < 3; i++) {
+  let nome = prompt(`Digite o ${i + 1}º nome (adicionado no final):`);
+  nomess.push(nome);
+}
+
+
+for (let i = 0; i < 3; i++) {
+  let nome = prompt(`Digite o ${i + 1}º nome (adicionado no início):`);
+  nomess.unshift(nome);
+}
+
+alert("Array completo: " + nomess.join(", "));
+
+
+nomess.shift(); 
+nomess.pop();  
+
+alert("Array final após remoções: " + nomess.join(", "));
